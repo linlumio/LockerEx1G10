89bde8c0955d9d8324556480a7714eabdc75a44a
Ale <akip-core@outlook.com>
2021-10-08 09:49:40 +0200

    first activity layout

--- app/src/main/java/com/example/lockerex1g10/MainActivity.kt
+++ app/src/main/java/com/example/lockerex1g10/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.lockerex1g10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    val accesso = mapOf<String, String>("pippo" to "goofy", "topolino" to "mickey", "paperino" to "donald", "batman" to "bruce", "ironman" to "stark", "alan" to "tnt")
+    //prima schermata
+    lateinit var bottone:Button
+    lateinit var nome:EditText
+    lateinit var pw:EditText
+    //seconda schermata
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottone = findViewById(R.id.log)
     }
 }
