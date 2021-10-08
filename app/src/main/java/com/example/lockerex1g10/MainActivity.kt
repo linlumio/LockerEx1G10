d38e48a16ec5ac8262deb2c465385cdbb85c52ae
Ale <akip-core@outlook.com>
2021-10-08 10:22:59 +0200

    init second activity

--- app/src/main/java/com/example/lockerex1g10/MainActivity.kt
+++ app/src/main/java/com/example/lockerex1g10/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.lockerex1g10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val accesso = mapOf<String, String>("pippo" to "goofy", "topolino" to "mickey", "paperino" to "donald", "batman" to "bruce", "ironman" to "stark", "alan" to "tnt")
@@ -12,11 +15,30 @@ class MainActivity : AppCompatActivity() {
     lateinit var nome:EditText
     lateinit var pw:EditText
     //seconda schermata
-    
+    lateinit var listona:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottone = findViewById(R.id.log)
+        nome = findViewById(R.id.user)
+        pw = findViewById(R.id.pws)
+
+        bottone.setOnClickListener{
+            var nm = nome.text.toString().lowercase()
+            var pssw = pw.text.toString()
+
+            if(accesso.get(nm)==pssw){
+
+            }else{
+                val toast = Toast.makeText(this,  "Utente o password errati" , Toast.LENGTH_LONG)
+                //the default toast view group is a relativelayout
+                val toastLayout = toast.getView() as ViewGroup
+                val toastTV =  toastLayout.getChildAt(0) as TextView
+                toastTV.setTextSize(30F)
+                toast.show()
+            }
+
+        }
     }
 }
