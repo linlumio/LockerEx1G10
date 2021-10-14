e0b5296e4c37148bc3466deab20f7166854abba2
Ale <akip-core@outlook.com>
2021-10-14 18:04:15 +0200

    cose

---
+++ app/src/main/java/com/example/lockerex1g10/AppPreferences.kt
@@ -0,0 +1,4 @@
+package com.example.lockerex1g10
+
+object AppPreferences {
+}
---
+++ app/src/main/java/com/example/lockerex1g10/TestFragment.kt
@@ -0,0 +1,11 @@
+package com.example.lockerex1g10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_fragment)
+    }
+}
